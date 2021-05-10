8d5380edc809cbb4b960c7365eb45fa09cfa0f6b
Wit, Allard de <allard.dewit@wur.nl>
2021-05-10 12:39:38 +0200

    Updated version to 1.0.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.0',  # Required
+    version='1.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -92,8 +92,6 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: science :: Meteorology',
-        'Topic :: science :: Agriculture',
 
         # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
